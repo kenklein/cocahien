ad619b94fdca003fd0525b4f456a0d017106119c
Kenenth Lee <k.klein@outlook.com>
2023-12-27 17:09:20 +0100

    Adding Sandbox features

---
+++ ken js sandbox/myscript.js
@@ -0,0 +1,17 @@
+function myFunction () {
+    document.getElementById("demo3").innerHTML = "Executed external JS code line";
+}
+
+function  myStockFunction () {
+    fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=PZRHDEAG8FRM7HAA')
+        .then(response => response.json())
+        .then(data => {
+            // Extract the stock price from the API response
+            const stockPrice = data['Global Quote']['05. price'];
+            // Display the stock price
+            document.getElementById('stockPrice').textContent = 'Apple Stock Price: ' + stockPrice;
+        })
+        .catch(error => {
+            console.log('Error fetching stock price:', error);
+        });
+}
