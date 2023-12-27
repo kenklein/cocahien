34ede877839a86d5c9364f2354d38e3a797bb19f
Kenenth Lee <k.klein@outlook.com>
2023-12-27 18:08:25 +0100

    change api stock BTC

--- ken js sandbox/myscript.js
+++ ken js sandbox/myscript.js
@@ -3,13 +3,13 @@ function myFunction () {
 }
 
 function  myStockFunction () {
-    fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=BTC&apikey=PZRHDEAG8FRM7HAA')
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BTC&apikey=PZRHDEAG8FRM7HAA')
         .then(response => response.json())
         .then(data => {
             // Extract the stock price from the API response
             const stockPrice = data['Global Quote']['05. price'];
             // Display the stock price
-            document.getElementById('stockPrice').textContent = 'Apple Stock Price: ' + stockPrice;
+            document.getElementById('stockPrice').textContent = 'BTC Stock Price: ' + stockPrice;
         })
         .catch(error => {
             console.log('Error fetching stock price:', error);
